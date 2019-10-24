84fb59a0ceccb12a7e28d2315ae89cc86dcdf50f
100134315 <100134315@lubbockisd.net>
2019-10-24 15:42:48 -0500

    10/24

--- StringsAndThings1.py
+++ StringsAndThings1.py
@@ -16,3 +16,29 @@ print(lastFirst)
 # Repetition
 
 print("Hip "*2 + "Hooray!")
+
+def rowYourBoat():
+    print("Row, "*3 + "your boat")
+    print("Gently down the stream")
+    print("merrily, "*4)
+    print("life is but a dream")
+
+rowYourBoat()
+
+# Indexing
+
+name = "Roy G Biv"
+firstChar = name[0]
+print(firstChar)
+
+middleCharIndex = len(name) // 2
+print(middleCharIndex)
+print(name[middleCharIndex])
+
+print(name[-3])
+
+for i in range(1, len(name)):
+    print(name[i])
+
+# Slicing and dicing
+
