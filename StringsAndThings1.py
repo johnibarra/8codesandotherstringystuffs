51b6f286af7bab9ab0508b9f958f6243fe5f3273
100134315 <100134315@lubbockisd.net>
2019-10-24 15:25:07 -0500

    10/24

---
+++ StringsAndThings1.py
@@ -0,0 +1,18 @@
+# srings
+
+# concatenation
+#    2 or more strings and put them together
+
+firstName = "wilma"
+lastName= "Flintstone"
+
+print(firstName + " " + lastName)
+
+name = firstName + " " + lastName
+lastFirst = lastName + ", " + firstName
+
+print(lastFirst)
+
+# Repetition
+
+print("Hip "*2 + "Hooray!")
