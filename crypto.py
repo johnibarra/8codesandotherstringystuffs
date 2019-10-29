7598b9d12ac8cc57b0ac0e98a1bb5013d1823d16
100134315 <100134315@lubbockisd.net>
2019-10-29 15:46:06 -0500

    10/29

--- crypto.py
+++ crypto.py
@@ -14,3 +14,8 @@ def scramble2Encrypt(plainText):
     cipherText = oddChars + evenChars
     return cipherText
 
+def scramble2Decrypt(cipherText):
+    halfLength = len(cipherText) // 2
+    evenChars = cipherText[halfLength:] # halflength to the end
+    oddChars = chiperText[:halflength]  # 0 to halflength -1
+    plainText
